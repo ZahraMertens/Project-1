49b64fbaac5492b4d1b38fcbfde3ac612d26975b
thossain <tanvirhossain2006@gmail.com>
2021-07-12 21:53:31 +1000

    initial javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,8 +18,23 @@ function goResult (event) {
 
   var name = $("#name").text().trim();
   console.log(name)
-  //window.location.replace("./result.html")
+  window.location.replace("./result.html")
 }
 
 searchButton.on("click", goResult);
 
+// api call for the cocktails by ingredients
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://the-cocktail-db.p.rapidapi.com/filter.php?i=Gin",//Type.. by ingredient
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "1a928d218emshf7ad00a62d285c5p1294e1jsn511010a300bf",
+		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
