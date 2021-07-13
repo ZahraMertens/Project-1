21e29fcd008851a4b6ef041cb202f945a21830ba
James Rostirolla <j_rostirolla@hotmail.com>
2021-07-13 13:25:02 +1000

    updated function calls to work with button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 
 var searchButton = $(".search-button");
-var inputIngredients = $("#autocomplete-input");
+var inputIngredients; //= document.getElementById("autocomplete-input").value;
 var nameInput = $("#name");
+var listCocktails = [];
+let ingredient;
 
 //Autocomplete dropdown for ingredients
 
@@ -14,21 +16,22 @@ $(document).ready(function(){
 });
 
 // Saving cocktail list in search
-
-var listCocktails = [];
-
+function searchMechanic() {  
+  var inputIngredients = document.getElementById("autocomplete-input").value;
 function storeCocktails() {
   localStorage.setItem("cocktails", JSON.stringify(listCocktails));
 }
 
 var formSubmitHandler = function (event) {
-  event.preventDefault();
+  //event.preventDefault();
 
-  var cocktailName = inputIngredients.value.trim();
+  var cocktailName = inputIngredients;
+  ingredient = cocktailName;
 
   if (cocktailName) {
     
-    getCocktailList(cocktailName);
+    // was blocking code, had to comment
+    // getCocktailList(cocktailName);
 
     
   } else {
@@ -37,10 +40,10 @@ var formSubmitHandler = function (event) {
 };
 
 // api call for the cocktails by ingredients
-Var cocktailName = function (ingredient) {
+function cocktailName(ingredient) {
   
 
-   fetch("https://the-cocktail-db.p.rapidapi.com/filter.php?i=" + ingredient, {
+   fetch(`https://the-cocktail-db.p.rapidapi.com/filter.php?i=${ingredient}`, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "1a928d218emshf7ad00a62d285c5p1294e1jsn511010a300bf",
@@ -49,7 +52,16 @@ Var cocktailName = function (ingredient) {
   })
      .then(function (response) {
       if (response.ok) {
-        window.location.replace("./result.html");
-        
-};
- 
+      window.location.replace("./result.html");  
+        };  
+      }) .then (function(data){
+          console.log(response.data);
+});
+     }
+     
+
+
+  storeCocktails();
+  formSubmitHandler();
+  cocktailName();
+}
